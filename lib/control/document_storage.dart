47517ad07d1ba2efb6228a52987cae39229dfc4b
Russell Tripp <russellptripp@gmail.com>
2025-01-18 16:20:11 -0500

    Move DocuimentStorage class into a separate dart file.

---
+++ lib/control/document_storage.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class DocumentStorage {
+  // temporary String, the currentDocument value will eventually be stored in shared prefs
+  String currentDocument = 'testfile.txt';
+  Future<String> get _localPath async {
+    final directory = await getExternalStorageDirectory();
+    return directory == null ? '' : directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/$currentDocument');
+  }
+
+  Future<File> writeFile(String contents) async {
+    final file = await _localFile;
+    return file.writeAsString('$contents\n', mode: FileMode.append);
+  }
+
+  Future<String> readFile() async {
+    try {
+      final file = await _localFile;
+      final contents = await file.readAsString();
+      return contents;
+    } catch (e) {
+      return 'The current document is empty.';
+    }
+  }
+}
