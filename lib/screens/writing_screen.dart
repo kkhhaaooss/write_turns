162c591a10d7eb637f9140522b6cbc2b703b5a9e
Russell Tripp <russellptripp@gmail.com>
2025-01-18 17:02:22 -0500

    Moved controller clear and file save to onChanged because onSubmitted stopped working after changing to multiline input.

--- lib/screens/writing_screen.dart
+++ lib/screens/writing_screen.dart
@@ -11,10 +11,10 @@ class EditorScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     bool isTextFieldEnabled = true;
     // will deal with focus later...
-    // late TextEditingController controller;
-    // controller = TextEditingController();
-    // late FocusNode textNode;
-    // textNode = FocusNode();
+    late TextEditingController controller;
+    controller = TextEditingController();
+    late FocusNode textNode;
+    textNode = FocusNode();
     String lastParagraph = ref.watch(lastParagraphProvider);
     return SizedBox(
       width: double.infinity,
@@ -28,26 +28,35 @@ class EditorScreen extends ConsumerWidget {
               minLines: 10,
               maxLines: 10,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                color: Colors.white,
-                width: 5.0,
-              ))),
-              // controller: controller,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 5.0,
+                  ),
+                ),
+              ),
+              controller: controller,
               canRequestFocus: true,
               autofocus: true,
-              // focusNode: textNode,
+              focusNode: textNode,
+              // Put clear and file save in onChanged because onSubmitted stopped working after changing to multiline textfield.
               onChanged: (String value) {
-                print(value == '\n' ? 'Enter detected, submit' : 'xXx');
+                if (value.contains(String.fromCharCode(13)) ||
+                    value.contains(String.fromCharCode(10))) {
+                  ref.read(lastParagraphProvider.notifier).state = value;
+                  storage.writeFile(value);
+                  controller.clear();
+                }
               },
               onSubmitted: (String value) {
                 ref.read(lastParagraphProvider.notifier).state = value;
                 storage.writeFile(value);
-                value = '';
+                controller.clear();
                 // textNode.requestFocus(); // pretty sure this needs to be a stateful widget for this to work
               },
             ),
-            Text('Another widget'),
+            Text('Timer widget goes here.'),
+            // Temporary solution to test saving files until I get save on Enter working again.
           ],
         ),
       ),
