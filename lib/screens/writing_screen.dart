0256348c9c8840d0fa9aa1804414c1641e9bb839
Russell Tripp <russellptripp@gmail.com>
2025-01-18 17:19:42 -0500

    Styling and alignment changes on writing screen

--- lib/screens/writing_screen.dart
+++ lib/screens/writing_screen.dart
@@ -20,40 +20,57 @@ class EditorScreen extends ConsumerWidget {
       width: double.infinity,
       child: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(lastParagraph),
-            TextField(
-              enabled: isTextFieldEnabled,
-              keyboardType: TextInputType.multiline,
-              minLines: 10,
-              maxLines: 10,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.white,
-                    width: 5.0,
-                  ),
+            Expanded(
+              flex: 1,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  lastParagraph,
+                  textAlign: TextAlign.left,
+                  softWrap: true,
+                  style: TextStyle(),
                 ),
               ),
-              controller: controller,
-              canRequestFocus: true,
-              autofocus: true,
-              focusNode: textNode,
-              // Put clear and file save in onChanged because onSubmitted stopped working after changing to multiline textfield.
-              onChanged: (String value) {
-                if (value.contains(String.fromCharCode(13)) ||
-                    value.contains(String.fromCharCode(10))) {
+            ),
+            Expanded(
+              flex: 1,
+              child: TextField(
+                enabled: isTextFieldEnabled,
+                keyboardType: TextInputType.multiline,
+                minLines: 10,
+                maxLines: 10,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 5.0,
+                    ),
+                  ),
+                ),
+                controller: controller,
+                canRequestFocus: true,
+                autofocus: true,
+                focusNode: textNode,
+                // Put clear and file save in onChanged because onSubmitted stopped working after changing to multiline textfield.
+                onChanged: (String value) {
+                  if (value.contains(String.fromCharCode(13)) ||
+                      value.contains(String.fromCharCode(10))) {
+                    ref.read(lastParagraphProvider.notifier).state = value;
+                    storage.writeFile(value);
+                    controller.clear();
+                  }
+                },
+                onSubmitted: (String value) {
                   ref.read(lastParagraphProvider.notifier).state = value;
                   storage.writeFile(value);
                   controller.clear();
-                }
-              },
-              onSubmitted: (String value) {
-                ref.read(lastParagraphProvider.notifier).state = value;
-                storage.writeFile(value);
-                controller.clear();
-                // textNode.requestFocus(); // pretty sure this needs to be a stateful widget for this to work
-              },
+                  // textNode.requestFocus(); // pretty sure this needs to be a stateful widget for this to work
+                },
+              ),
             ),
             Text('Timer widget goes here.'),
             // Temporary solution to test saving files until I get save on Enter working again.
